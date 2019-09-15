7cde1b362700303f01cdb644e179de7d5fa76b37
simonayele <simonayele@gmail.com>
2019-09-15 17:18:29 +0000

    payment processing set up with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,20 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_current_course
   def show
   end
 
    private
 
-  helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
+   def require_enrollment_for_current_course
+      @course = current_lesson.section.course
+      if !current_user.enrolled_in?(@course)
+        redirect_to course_path(@course), alert: 'Please enrol for the course'
+      end
+    end
+
+    helper_method :current_lesson
+    def current_lesson
+      @current_lesson ||= Lesson.find(params[:id])
+    end
 end
