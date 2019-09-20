a89625aa67729322e7f4d06b7a20a6141d84d734
simonayele <simonayele@gmail.com>
2019-09-20 04:26:00 +0000

    sections drag and drop

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -37,7 +37,11 @@ class Instructor::SectionsController < ApplicationController
 
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+     @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
   end
 
   def section_params
